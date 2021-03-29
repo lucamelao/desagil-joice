1aa28099ce8ab22e591dd3514b2133b3ea79cb97
matkwon <62735876+matkwon@users.noreply.github.com>
2021-03-29 18:57:35 -0300

    Update CalculadoraCFC.java
    conta errada

--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -9,7 +9,6 @@ public class CalculadoraCFC extends Calculadora {
 	
 	@Override
 	public double calcula(double peso, double raio){
-		return (4*peso)/(Math.pow(2*raio*(Math.pow(2, 0.5)), 3)/(Math.pow(Math.pow(3, 0.5), 3))*6.022*100);
+		return (4*peso)/(Math.pow(2*raio*(Math.pow(2, 0.5)), 3)*6.022*100);
 	}
 }
-
